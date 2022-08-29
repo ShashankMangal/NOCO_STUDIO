4cb2768a6cc73ae65423456f4a5c4c8cc7b43e6e
shashankmangal10@gmail.com <Sunil@1234>
2022-08-29 22:01:28 +0530

    facebook ads added mediation commit

--- app/src/main/java/com/sharkBytesLab/nocostudio/Misc/MyApplication.java
+++ app/src/main/java/com/sharkBytesLab/nocostudio/Misc/MyApplication.java
@@ -17,7 +17,7 @@ public class MyApplication extends Application
         super.onCreate();
 
         // Enable verbose OneSignal logging to debug issues if needed.
-       OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
 
         // OneSignal Initialization
         OneSignal.initWithContext(this);
