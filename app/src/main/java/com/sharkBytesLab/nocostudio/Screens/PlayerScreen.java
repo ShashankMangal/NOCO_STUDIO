afb2614812ab4256180afe6fa2cf851ebc3efa6b
Shashank Mangal <shashankmangal10@gmail.com>
2022-10-08 18:10:55 +0530

    music player screen time and seekbar ready

--- app/src/main/java/com/sharkBytesLab/nocostudio/Screens/PlayerScreen.java
+++ app/src/main/java/com/sharkBytesLab/nocostudio/Screens/PlayerScreen.java
@@ -1,14 +1,29 @@
 package com.sharkBytesLab.nocostudio.Screens;
 
+import static com.sharkBytesLab.nocostudio.Fragments.MusicFragment.musicFiles;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.SeekBar;
 
+import com.sharkBytesLab.nocostudio.Misc.MusicFiles;
+import com.sharkBytesLab.nocostudio.R;
 import com.sharkBytesLab.nocostudio.databinding.ActivityPlayerScreenBinding;
+
+import java.util.ArrayList;
 
 public class PlayerScreen extends AppCompatActivity
 {
     private ActivityPlayerScreenBinding binding;
+    private int position = -1;
+    private static ArrayList<MusicFiles> listSongs = new ArrayList<>();
+    private static Uri uri;
+    private static MediaPlayer mediaPlayer;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +31,89 @@ public class PlayerScreen extends AppCompatActivity
         binding = ActivityPlayerScreenBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        getIntentMethod();
 
+        binding.seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+            {
+                if(mediaPlayer != null && fromUser)
+                {
+                    mediaPlayer.seekTo(progress*1000);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        PlayerScreen.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run()
+            {
+                if(mediaPlayer!=null)
+                {
+                    int mCurrentPosition = mediaPlayer.getCurrentPosition() / 1000;
+                    binding.seekBar.setProgress(mCurrentPosition);
+                    binding.durationPlayed.setText(formattedTime(mCurrentPosition));
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
 
     }
+
+    private String formattedTime(int mCurrentTime)
+    {
+        String totalOut = "";
+        String totalNew = "";
+        String seconds = String.valueOf(mCurrentTime%60);
+        String minutes = String.valueOf(mCurrentTime/60);
+
+        totalOut = minutes + ":" + seconds;
+        totalNew = minutes + ":" + "0" + seconds;
+
+        if(seconds.length() == 1)
+        {
+            return totalNew;
+        }
+        else
+        {
+            return totalOut;
+        }
+    }
+
+    private void getIntentMethod()
+    {
+        position = getIntent().getIntExtra("position", -1);
+        listSongs = musicFiles;
+        if(listSongs != null)
+        {
+            binding.playPause.setImageResource(R.drawable.ic_pause);
+            uri = Uri.parse(listSongs.get(position).getPath());
+        }
+
+        if(mediaPlayer != null)
+        {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
+            mediaPlayer.start();
+        }
+        else
+        {
+            mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
+            mediaPlayer.start();
+        }
+
+        binding.seekBar.setMax(mediaPlayer.getDuration() / 1000);
+
+    }
+
 }
