196a86aac0af6fc26e67b8b8310d0e0997a7375e
Shashank Mangal <shashankmangal10@gmail.com>
2022-10-12 18:04:47 +0530

    player update and error solved

--- app/src/main/java/com/sharkBytesLab/nocostudio/Screens/PlayerScreen.java
+++ app/src/main/java/com/sharkBytesLab/nocostudio/Screens/PlayerScreen.java
@@ -4,17 +4,23 @@ import static com.sharkBytesLab.nocostudio.Fragments.MusicFragment.*;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.palette.graphics.Palette;
+
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -46,10 +52,28 @@ public class PlayerScreen extends AppCompatActivity implements MediaPlayer.OnCom
         getIntentMethod();
         binding.songName.setText(listSongs.get(position).getTitle());
         binding.songArtist.setText("Artist : " + listSongs.get(position).getArtist());
-        mediaPlayer.setOnCompletionListener(this);
 
-        binding.songName.setEllipsize(TextUtils.TruncateAt.MARQUEE);
-        binding.songName.setSelected(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+        {
+        Window window = getWindow();
+        WindowManager.LayoutParams winParams = window.getAttributes();
+        winParams.flags &= ~WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        window.setAttributes(winParams);
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
+            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
+        }
+        if (Build.VERSION.SDK_INT >= 19) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+
+        mediaPlayer.setOnCompletionListener(this);
 
         binding.seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -121,8 +145,19 @@ public class PlayerScreen extends AppCompatActivity implements MediaPlayer.OnCom
             }
         });
 
-        binding.backBtn.setOnClickListener(v-> onBackPressed());
+        binding.playerBack.setOnClickListener(v-> onBackPressed());
 
+    }
+
+    public static void setWindowFlag(Activity activity, final int bits, boolean on) {
+        Window win = activity.getWindow();
+        WindowManager.LayoutParams winParams = win.getAttributes();
+        if (on) {
+            winParams.flags |= bits;
+        } else {
+            winParams.flags &= ~bits;
+        }
+        win.setAttributes(winParams);
     }
 
     @Override
@@ -454,10 +489,10 @@ public class PlayerScreen extends AppCompatActivity implements MediaPlayer.OnCom
                     Palette.Swatch swatch = palette.getDominantSwatch();
                     if(swatch!=null)
                     {
-                        binding.imageViewGradient.setBackgroundResource(R.drawable.cover_art_shadow);
+                        //binding.imageViewGradient.setBackgroundResource(R.drawable.cover_art_shadow);
                         binding.mContainer.setBackgroundResource(R.drawable.main_bg);
                         GradientDrawable gradientDrawable = new GradientDrawable(GradientDrawable.Orientation.BOTTOM_TOP, new int[]{swatch.getRgb(), 0x00000000});
-                        binding.imageViewGradient.setBackground(gradientDrawable);
+                        //binding.imageViewGradient.setBackground(gradientDrawable);
 
                         GradientDrawable gradientDrawableBg = new GradientDrawable(GradientDrawable.Orientation.BOTTOM_TOP, new int[]{swatch.getRgb(), 0x00000000});
                         binding.mContainer.setBackground(gradientDrawableBg);
@@ -466,10 +501,10 @@ public class PlayerScreen extends AppCompatActivity implements MediaPlayer.OnCom
                     }
                     else
                     {
-                        binding.imageViewGradient.setBackgroundResource(R.drawable.cover_art_shadow);
+                        //binding.imageViewGradient.setBackgroundResource(R.drawable.cover_art_shadow);
                         binding.mContainer.setBackgroundResource(R.drawable.main_bg);
                         GradientDrawable gradientDrawable = new GradientDrawable(GradientDrawable.Orientation.BOTTOM_TOP, new int[]{0xff000000, 0x00000000});
-                        binding.imageViewGradient.setBackground(gradientDrawable);
+                        //binding.imageViewGradient.setBackground(gradientDrawable);
 
                         GradientDrawable gradientDrawableBg = new GradientDrawable(GradientDrawable.Orientation.BOTTOM_TOP, new int[]{0xff000000, 0xff000000});
                         binding.mContainer.setBackground(gradientDrawableBg);
@@ -480,8 +515,8 @@ public class PlayerScreen extends AppCompatActivity implements MediaPlayer.OnCom
         else
         {
             Glide.with(this).asBitmap().load(R.drawable.player_cover).into(binding.coverArt);
-            binding.imageViewGradient.setBackgroundResource(R.drawable.cover_art_shadow);
-            binding.mContainer.setBackgroundResource(R.drawable.main_bg);
+            //binding.imageViewGradient.setBackgroundResource(R.drawable.cover_art_shadow);
+            binding.mContainer.setBackgroundResource(R.drawable.player_gradient);
         }
     }
 
