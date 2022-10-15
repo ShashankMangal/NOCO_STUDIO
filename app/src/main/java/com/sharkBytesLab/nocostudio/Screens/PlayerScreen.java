f8f59a501b622936ca7409d7e891a7c0c3d3a70e
Shashank Mangal <shashankmangal10@gmail.com>
2022-10-15 17:00:02 +0530

    notification working

--- app/src/main/java/com/sharkBytesLab/nocostudio/Screens/PlayerScreen.java
+++ app/src/main/java/com/sharkBytesLab/nocostudio/Screens/PlayerScreen.java
@@ -3,12 +3,19 @@ package com.sharkBytesLab.nocostudio.Screens;
 import static com.sharkBytesLab.nocostudio.Adapters.AlbumDetailsAdapter.albumFiles;
 import static com.sharkBytesLab.nocostudio.Adapters.MusicAdapter.mFiles;
 import static com.sharkBytesLab.nocostudio.Fragments.MusicFragment.*;
+import static com.sharkBytesLab.nocostudio.Misc.MyApplication.ACTION_PLAY;
+import static com.sharkBytesLab.nocostudio.Misc.MyApplication.ACTION_PREVIOUS;
+import static com.sharkBytesLab.nocostudio.Misc.MyApplication.CHANNEL_ID_2;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.palette.graphics.Palette;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -24,6 +31,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
+import android.support.v4.media.session.MediaSessionCompat;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -38,6 +46,7 @@ import com.bumptech.glide.Glide;
 import com.sharkBytesLab.nocostudio.Misc.ActionPlaying;
 import com.sharkBytesLab.nocostudio.Misc.MusicFiles;
 import com.sharkBytesLab.nocostudio.Misc.MusicService;
+import com.sharkBytesLab.nocostudio.Misc.NotificationReceiver;
 import com.sharkBytesLab.nocostudio.R;
 import com.sharkBytesLab.nocostudio.databinding.ActivityPlayerScreenBinding;
 
@@ -53,15 +62,15 @@ public class PlayerScreen extends AppCompatActivity implements ActionPlaying, Se
     private Handler handler = new Handler();
     private Thread playThread, prevThread, nextThread;
     MusicService musicService;
+    MediaSessionCompat mediaSessionCompat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityPlayerScreenBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        mediaSessionCompat = new MediaSessionCompat(getBaseContext(), "NOCO Studio");
         getIntentMethod();
-
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             Window window = getWindow();
@@ -188,6 +197,7 @@ public class PlayerScreen extends AppCompatActivity implements ActionPlaying, Se
     public void playPauseBtnClicked() {
         if (musicService.isPlaying()) {
             binding.playPause.setImageResource(R.drawable.playnow_icon);
+            showNotification(R.drawable.playnow_icon);
             musicService.pause();
             binding.seekBar.setMax(musicService.getDuration() / 1000);
 
@@ -203,6 +213,7 @@ public class PlayerScreen extends AppCompatActivity implements ActionPlaying, Se
             });
         } else {
             binding.playPause.setImageResource(R.drawable.ic_pause);
+            showNotification(R.drawable.ic_pause);
             musicService.start();
             binding.seekBar.setMax(musicService.getDuration() / 1000);
 
@@ -263,6 +274,7 @@ public class PlayerScreen extends AppCompatActivity implements ActionPlaying, Se
                 }
             });
             musicService.onCompleted();
+            showNotification(R.drawable.ic_pause);
             binding.playPause.setBackgroundResource(R.drawable.ic_pause);
             musicService.start();
         } else {
@@ -292,6 +304,7 @@ public class PlayerScreen extends AppCompatActivity implements ActionPlaying, Se
                 }
             });
             musicService.onCompleted();
+            showNotification(R.drawable.playnow_icon);
             binding.playPause.setBackgroundResource(R.drawable.playnow_icon);
         }
     }
@@ -345,6 +358,7 @@ public class PlayerScreen extends AppCompatActivity implements ActionPlaying, Se
                 }
             });
             musicService.onCompleted();
+            showNotification(R.drawable.ic_pause);
             binding.playPause.setBackgroundResource(R.drawable.ic_pause);
             musicService.start();
         } else {
@@ -374,6 +388,7 @@ public class PlayerScreen extends AppCompatActivity implements ActionPlaying, Se
                 }
             });
             musicService.onCompleted();
+            showNotification(R.drawable.playnow_icon);
             binding.playPause.setBackgroundResource(R.drawable.playnow_icon);
         }
     }
@@ -408,7 +423,7 @@ public class PlayerScreen extends AppCompatActivity implements ActionPlaying, Se
             binding.playPause.setImageResource(R.drawable.ic_pause);
             uri = Uri.parse(listSongs.get(position).getPath());
         }
-
+        showNotification(R.drawable.ic_pause);
         Intent intent = new Intent(this, MusicService.class);
         intent.putExtra("servicePosition", position);
         startService(intent);
@@ -515,4 +530,56 @@ public class PlayerScreen extends AppCompatActivity implements ActionPlaying, Se
     {
         musicService = null;
     }
+
+    void showNotification(int playPauseBtn)
+    {
+        Intent intent = new Intent(this, PlayerScreen.class);
+        PendingIntent contentIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+
+        Intent prevIntent = new Intent(this, NotificationReceiver.class).setAction(ACTION_PREVIOUS);
+        PendingIntent prevPending = PendingIntent.getBroadcast(this, 0, prevIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Intent pauseIntent = new Intent(this, NotificationReceiver.class).setAction(ACTION_PLAY);
+        PendingIntent pausePending = PendingIntent.getBroadcast(this, 0, pauseIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Intent nextIntent = new Intent(this, NotificationReceiver.class).setAction(ACTION_PREVIOUS);
+        PendingIntent nextPending = PendingIntent.getBroadcast(this, 0, nextIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        byte[] picture = null;
+        picture = getAlbumArt(musicFiles.get(position).getPath());
+
+        Bitmap thumb = null;
+        if(picture!=null)
+        {
+            thumb = BitmapFactory.decodeByteArray(picture, 0, picture.length);
+        }
+        else
+        {
+            thumb = BitmapFactory.decodeResource(getResources(), R.drawable.album_cover_art);
+        }
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID_2)
+                .setSmallIcon(playPauseBtn).setLargeIcon(thumb).setContentTitle(musicFiles.get(position).getTitle())
+                .setContentText(musicFiles.get(position).getArtist())
+                .addAction(io.github.dreierf.materialintroscreen.R.drawable.ic_previous, "Previous", prevPending)
+                .addAction(playPauseBtn, "Pause", pausePending)
+                .addAction(io.github.dreierf.materialintroscreen.R.drawable.ic_next, "Next", nextPending)
+                .setStyle(new androidx.media.app.NotificationCompat.MediaStyle()
+                .setMediaSession(mediaSessionCompat.getSessionToken()))
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setOnlyAlertOnce(true)
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification);
+    }
+
+    private byte[] getAlbumArt(String uri)
+    {
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(uri);
+        byte[] art = retriever.getEmbeddedPicture();
+        retriever.release();
+        return art;
+    }
+
 }
